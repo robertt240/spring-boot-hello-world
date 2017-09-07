8e6fc117b4997fb65e9f94b67e0d79224eba01fd
robertt240 <robertkasperczyk@gmail.com>
2017-09-07 11:33:13 +0200

    initial commit

---
+++ src/main/java/com/example/springboothelloworld/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.example.springboothelloworld.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String getIndexPage() {
+        return "index";
+    }
+}
